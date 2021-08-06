6ee588dab00df3837bc346e714db4ca9f719c6e3
RAJIVRAWAL <88193056+RAJIVRAWAL@users.noreply.github.com>
2021-08-06 13:40:09 +0530

    Revert "Update sketch.js"

--- sketch.js
+++ sketch.js
@@ -5,27 +5,26 @@
  function preload(){
   //pre-load images
     RunnerImg = loadAnimation("Runner-1.png","Runner-2.png");
- //correction done
-    pathImg = loadImage("path.png");
+    RunnerImg = loadImage("Runner-1.png","Runner-2.png");
+    pathImg = loadImage(path.png);
   }
 
   function setup(){
     createCanvas(400,400);
   //create sprites here
      path = createSprite(200,200);
-    path.addImage(pathImg);
-    
+     path.addImage(pathImg);
      path.velocityY = 4;
      path.scale = 1.2
 
      Runner = createSprite(130,200,30,30);
-     Runner.addAnimation("RunnerImg1",RunnerImg);
-     Runner.scale = 0.1
+     Runner.addAnimation("RunnerImg1",RunnerImg1);
+     Runner.scale = 0.25
 
-      boundary1 = createSprite(10,200,50,400);
+      boundary1 = createSprite(10,200,400,20);
       boundary1.visible = false
 
-      boundary2 = createSprite(380,200,50,400)
+      boundary2 = createSprite(390,210,400,20)
       boundary2.visible = false
   }
 
@@ -40,6 +39,6 @@
 }
 
       Runner.collide(boundary1,boundary2);
-//corection done
-     drawSprites();
+
+     drawSprite();
 }
